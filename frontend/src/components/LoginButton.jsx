6477238eb06a9b19a5c79c85f11868a5060f595d
Eve Lin <evelin@buffalo.edu>
2025-06-15 01:51:04 -0400

    Animated Buttons

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -1,19 +1,17 @@
-import React from "react"
-import { useNavigate } from "react-router-dom"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/Button.css"; 
 
 const LoginButton = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <img
-        src={`${import.meta.env.BASE_URL}LoginButton.png`}
-        alt="Login"
-        onClick={() => navigate("/login")}
-        className="cursor-pointer"
-        style={{
-            width: "clamp(100px, 30vw, 300px)" //  scales from 100px to 300px
-        }}
+      src={`${import.meta.env.BASE_URL}LoginButton.png`}
+      alt="Login"
+      onClick={() => navigate("/login")}
+      className="button"
     />
-  )
-}
+  );
+};
 
-export default LoginButton
+export default LoginButton;
--- frontend/src/components/RegisterButton.jsx
+++ frontend/src/components/RegisterButton.jsx
@@ -1,19 +1,17 @@
-import React from "react"
-import { useNavigate } from "react-router-dom"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/Button.css"; 
 
 const RegisterButton = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <img
-        src={`${import.meta.env.BASE_URL}RegisterButton.png`}
-        alt="Login"
-        onClick={() => navigate("/register")}
-        className="cursor-pointer"
-        style={{
-            width: "clamp(100px, 30vw, 300px)" //  scales from 100px to 300px
-        }}
+      src={`${import.meta.env.BASE_URL}RegisterButton.png`}
+      alt="Register"
+      onClick={() => navigate("/register")}
+      className="button"
     />
-  )
-}
+  );
+};
 
-export default RegisterButton
+export default RegisterButton;
